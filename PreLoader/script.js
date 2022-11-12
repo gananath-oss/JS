edc5036813c9c8730f6dc564c9ca68ac1011f752
gananath-oss <56883476+gananath-oss@users.noreply.github.com>
2022-11-11 19:54:55 -0800

    used deelay.me  for get deelay

---
+++ PreLoader/script.js
@@ -0,0 +1,5 @@
+// querySelector eken gane api select karana class os tag eka ektama hanbena tena
+
+window.addEventListener("load", function(){
+    document.querySelector(".loader").classList.add("hidden");
+})
